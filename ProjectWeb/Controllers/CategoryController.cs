e48fe4f0447140d4d2d1c01c867b03f72a36c8e4
Pham Thanh Son <sonpt28@fpt.edu.vn>
2024-11-08 11:16:43 +0700

    Finish CRUD operation of Category

--- ProjectWeb/Controllers/CategoryController.cs
+++ ProjectWeb/Controllers/CategoryController.cs
@@ -16,9 +16,61 @@ namespace ProjectWeb.Controllers
 			List<Category> listCategory=_dbContext.Categories.ToList();
 			return View(listCategory);
 		}
-		public IActionResult Edit(int id)
+		public IActionResult Add()
 		{
 			return View();
+		}
+		[HttpPost]
+		public IActionResult Add(Category category)
+		{
+			if (category.Name.Equals(category.Description))
+			{
+				ModelState.AddModelError("Name", "Name should be different than Description");
+			}
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Add(category);
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Edit(int id)
+		{
+			Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Update(category);
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int id)
+		{
+			Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Delete(Category category)
+		{
+				_dbContext.Categories.Remove(category);
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+
 		}
 	}
 }
