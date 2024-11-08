0e53f90caa3d308f25688bdab4179e3fcb0c6015
Pham Thanh Son <sonpt28@fpt.edu.vn>
2024-11-08 08:37:51 +0700

    Seeding category table

--- ProjectWeb/Data/ApplicationDBContext.cs
+++ ProjectWeb/Data/ApplicationDBContext.cs
@@ -7,5 +7,16 @@ namespace ProjectWeb.Data
 	{
 		public DbSet<Category> Categories { get; set; }
 		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Category>().HasData(
+				new Category { Id = 1, Name = "Adventure",Description="Give you a feel of challenge",DisplayPriority = 1},
+				new Category { Id = 2, Name = "Roman", Description = "So romantique", DisplayPriority = 4 },
+				new Category { Id = 3, Name = "Horror", Description = "So scary", DisplayPriority = 3 },
+				new Category { Id = 4, Name = "Scifi", Description = "So interesting", DisplayPriority = 2 }
+				);
+
+		}
 	}
 }
--- ProjectWeb/Migrations/20241108013538_SeedingCategoryTable.Designer.cs
+++ ProjectWeb/Migrations/20241108013538_SeedingCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectWeb.Data;
 
@@ -10,9 +11,11 @@ using ProjectWeb.Data;
 namespace ProjectWeb.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241108013538_SeedingCategoryTable")]
+    partial class SeedingCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
